123fce9dd2740e6d99352c0b8c34c9c3321fb6ca
Sorokin Aleksei <alexey-sor@yandex.ru>
2022-10-21 00:59:40 +0300

    \\

--- Task_2.17/src/interfaces/Bird.java
+++ Task_2.17/src/interfaces/Bird.java
@@ -1,8 +1,18 @@
 package interfaces;
 
-public class Bird extends Animal {
+public class Bird extends Animal implements AbleToFly, AbleToRun {
     @Override
     public void eat() {
         System.out.println("bird eat");
+    }
+
+    @Override
+    public void Fly() {
+        System.out.println("bird eat");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("bird run");
     }
 }
