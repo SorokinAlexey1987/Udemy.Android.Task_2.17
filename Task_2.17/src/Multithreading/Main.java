46c64eb16ac89445d366ff8ce25c437af958786e
Sorokin Aleksei <alexey-sor@yandex.ru>
2022-11-01 22:37:07 +0300

    \\

--- Task_2.17/src/Multithreading/Main.java
+++ Task_2.17/src/Multithreading/Main.java
@@ -2,11 +2,33 @@ package Multithreading;
 
 public class Main {
     public static void main(String[] args) {
-        RunnableTimer runnableTimer = new RunnableTimer();
-        Thread timer = new Thread(runnableTimer);
+        Thread timer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for(int i = 0; i < 10; i++){
+                        System.out.println(1);
+                        Thread.sleep(1000);
+                    }
+                } catch (Exception e) {
+
+                }
+            }
+        });
         timer.start();
-        for(int i = 0; i < 1000000; i++) {
-            System.out.println(2);
-        }
+        Thread timer2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for(int i = 0; i < 10; i++){
+                        System.out.println(2);
+                        Thread.sleep(1000);
+                    }
+                } catch (Exception e) {
+
+                }
+            }
+        });
+        timer2.start();
     }
 }
