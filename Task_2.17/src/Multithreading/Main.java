5adf921936e1a0cd33609c1cfe049342b54d5461
Sorokin Aleksei <alexey-sor@yandex.ru>
2022-11-01 22:54:39 +0300

    \\

--- Task_2.17/src/Multithreading/Main.java
+++ Task_2.17/src/Multithreading/Main.java
@@ -1,10 +1,14 @@
 package Multithreading;
 
-public class Main {
+import org.w3c.dom.ls.LSOutput;
 
+public class Main {
     static boolean isFive = false;
 
     public static void main(String[] args) {
+        int j = (int)(Math.random() * 10);
+        System.out.println(j);
+
         Thread timer = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -24,22 +28,6 @@ public class Main {
             }
         });
         timer.start();
-
-        Thread timer2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    int i = 0;
-                    while(!isFive) {
-                        System.out.println(i);
-                        i++;
-                        Thread.sleep(1000);
-                    }
-                } catch (Exception e) {
-
-                }
-            }
-        });
-        timer2.start();
     }
+
 }
